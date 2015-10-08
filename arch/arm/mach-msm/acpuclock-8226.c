793b2d4426976854509b8da591605643e05e4b06
KingKaminari <ialbuquerqueb792@gmail.com>
2015-10-08 12:25:17 -0300

    ?

--- arch/arm/mach-msm/acpuclock-8226.c
+++ arch/arm/mach-msm/acpuclock-8226.c
@@ -60,6 +60,12 @@ static struct msm_bus_scale_pdata bus_client_pdata = {
 };
 
 static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p1[] = {
+<<<<<<< HEAD
+=======
+	{ 1,   96000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
+	{ 1,  192000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
+	{ 1,  249600, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
+>>>>>>> cbbb70d... arm: dts: Force CPU underclocking
 	{ 1,  300000, PLL0,    4, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  384000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  600000, PLL0,    4, 0,   CPR_CORNER_4, 0, 6 },
@@ -71,6 +77,12 @@ static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p1[] = {
 };
 
 static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p2[] = {
+<<<<<<< HEAD
+=======
+	{ 1,   96000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
+	{ 1,  192000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
+	{ 1,  249600, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
+>>>>>>> cbbb70d... arm: dts: Force CPU underclocking
 	{ 1,  300000, PLL0,    4, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  384000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  600000, PLL0,    4, 0,   CPR_CORNER_4, 0, 6 },
@@ -82,6 +94,12 @@ static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p2[] = {
 };
 
 static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p4[] = {
+<<<<<<< HEAD
+=======
+	{ 1,   96000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
+	{ 1,  192000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
+	{ 1,  249600, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
+>>>>>>> cbbb70d... arm: dts: Force CPU underclocking
 	{ 1,  300000, PLL0,    4, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  384000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  600000, PLL0,    4, 0,   CPR_CORNER_4, 0, 6 },
@@ -96,6 +114,12 @@ static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p4[] = {
 };
 
 static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p6[] = {
+<<<<<<< HEAD
+=======
+	{ 1,   96000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
+	{ 1,  192000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
+	{ 1,  249600, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
+>>>>>>> cbbb70d... arm: dts: Force CPU underclocking
 	{ 1,  300000, PLL0,    4, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  384000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  600000, PLL0,    4, 0,   CPR_CORNER_4, 0, 6 },
